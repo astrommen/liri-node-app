41df2c8298a0394afe6c4f40c41922d366b61934
Aja Strommen <9466667+astrommen@users.noreply.github.com>
2020-03-02 18:50:02 -0500

    default song is parsed & working

--- liri.js
+++ liri.js
@@ -29,24 +29,41 @@ if (process.argv[2]==="concert-this"){
         }
     );
 } else if (process.argv[2]==="spotify-this-song"){
-    var song = process.argv.slice(3).join(" ");
-    console.log(song);
-    spotify.search({type: "track", query: song},
-    function(err, data){
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        };
-
+    if(process.argv[3]!=null){
+        var song = process.argv.slice(3).join(" ");
         var i = 0;
-        data.tracks.items.forEach(element => {
-            if (element.preview_url != null){// && i < 1){
-                i++;
-                console.log(i);
-                console.log("Artist: " + element.artists[0].name);
-                console.log("Song: " + element.name);
-                console.log("Album: " + element.album.name);
-                console.log("Preview Link: " + element.preview_url);
-            }
-        })
-    });
-}
+        console.log(song);
+        spotify.search({type: "track", query: song},
+        function(err, data){
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            };
+            data.tracks.items.forEach(element => {
+                if (element.preview_url != null && i<1){
+                    i++
+                    console.log(element);
+                    console.log(
+                        "#"+ i + "\nArtist: " + element.artists[0].name +
+                        "\nSong: " + element.name +
+                        "\nAlbum: " + element.album.name +
+                        "\nPreview Link: " + element.preview_url
+                    );
+                    
+                };
+            });
+        });
+    } else{
+        console.log("no input");
+        spotify.search({type: "track", query: "The Sign"},
+        function(err,data){
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            };
+            var defaulted = data.tracks.items[0];
+            console.log("\nArtist: " + defaulted.artists[0].name +
+            "\nSong: " + defaulted.name +
+            "\nAlbum: " + defaulted.album.name +
+            "\nPreview Link: " + defaulted.preview_url + "\n");
+        });
+    };
+};
